43453af1bc1f0c84abd26de1b8bd5f036506d240
tactnician <tactnician@gmail.com>
2021-03-01 17:46:42 -0500

    added database files

--- api/server.js
+++ api/server.js
@@ -4,36 +4,19 @@ require('dotenv').config();
 const server = express();
 const helmet = require('helmet');
 
-//routers 
+// define routers 
 const stripe = require('../router/stripe-route'); 
 const binance = require('../router/binance-route');
 const wallet = require('../router/wallet-route')
 
+//use dependencies
 server.use(helmet());
 server.use(express.json());
+server.use(express.static("."))
 
-
-
+//assign urls to routers
 server.use('/api/pay', stripe);
-server.use('/api/trade', binance);
-server.use('/api/wallet', wallet)
-
-
-server.get('/', (req, res) => {
-    res.send(`
-        <h1>Api Portal</h1>
-        <h3>Endpoint:</h3>
-        <p></p>
-        <p>~/api/projects</p>
-        <p></p>
-        <h3>Methods </h3>
-        <p>GET projects @ '/'</p>
-        <p>GET projects by id @ '/:id' </p>
-        <p>POST projects @ '/' </p>
-        <p>GET action items by id @ '/:id/actions'</p>
-        <p>POST action item @ '/actions'</p>
-    `)
-})
-
+// server.use('/api/trade', binance);
+// server.use('/api/wallet', wallet)
 
 module.exports = server; 
