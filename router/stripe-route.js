6b47d90be7398d999e7f77e27dce55de4addcca6
tactnician <tactnician@gmail.com>
2021-03-03 21:50:28 -0500

    added /new-order to stripe route

--- router/stripe-route.js
+++ router/stripe-route.js
@@ -21,8 +21,44 @@ router.post('/create-payment-intent', async(req, res) => {
         });
         res.status(200).json();
     } catch (error) {
-        res.status(500).json(`error creating resource ${error}`)
+        res.status(500).json(`error posting pay intent ${error}`)
     }    
 })
+
+router.post('/create-checkout-session', async(req,res) => {
+    try {
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            line_items: [
+              {
+                price_data: {
+                  currency: 'usd',
+                  product_data: {
+                    name: 'T-shirt',
+                  },
+                  unit_amount: 2000,
+                },
+                quantity: 1,
+              },
+            ],
+            mode: 'payment',
+            success_url: 'https://example.com/success',
+            cancel_url: 'https://example.com/cancel',
+          });
+        
+          res.json({ id: session.id });
+    } catch (error) {
+        res.status(500).json(`error creating pay session ${error}`)
+    }
+})
+
+router.post('/new-order', async(req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+})
+
 
 module.exports = router;
